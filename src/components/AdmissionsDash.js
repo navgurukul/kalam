f8d387dfd555b0763f106310b0d0e3f07475876e
pysaquib <iamsaquibnasim@gmail.com>
2020-12-07 10:37:41 +0530

    Feedback shown without login, qualification mapped to readable format

--- src/components/AdmissionsDash.js
+++ src/components/AdmissionsDash.js
@@ -1,59 +1,63 @@
-import 'date-fns';
-import React from 'react';
+import "date-fns";
+import React from "react";
 // import { allStages} from '../config';
-import { connect } from 'react-redux';
-import DateFnsUtils from '@date-io/date-fns';
-import { withStyles, MuiThemeProvider } from '@material-ui/core/styles';
-import Select from 'react-select';
+import { connect } from "react-redux";
+import DateFnsUtils from "@date-io/date-fns";
+import { withStyles, MuiThemeProvider } from "@material-ui/core/styles";
+import Select from "react-select";
 
-import axios from 'axios';
-import Box from '@material-ui/core/Box';
-import makeAnimated from 'react-select/animated';
+import axios from "axios";
+import Box from "@material-ui/core/Box";
+import makeAnimated from "react-select/animated";
 
-import { theme } from '../theme/theme';
-import { changeFetching, setupUsers } from '../store/actions/auth';
-import { withRouter } from 'react-router-dom';
-import StudentService from '../services/StudentService';
-import { MuiPickersUtilsProvider, KeyboardDatePicker } from '@material-ui/pickers';
-import { allStages } from '../config';
-import MainLayout from './MainLayout';
+import { theme } from "../theme/theme";
+import { changeFetching, setupUsers } from "../store/actions/auth";
+import { withRouter } from "react-router-dom";
+import StudentService from "../services/StudentService";
+import {
+  MuiPickersUtilsProvider,
+  KeyboardDatePicker,
+} from "@material-ui/pickers";
+import { allStages } from "../config";
+import MainLayout from "./MainLayout";
 
 const animatedComponents = makeAnimated();
 // API USage : https://blog.logrocket.com/patterns-for-data-fetching-in-react-981ced7e5c56/
 const baseURL = process.env.API_URL;
-const allStagesOptions = Object.keys(allStages).map(x => { return { value: x, label: allStages[x] } });
-allStagesOptions.push({ value: 'default', label: 'Back To All Students Details' })
+const allStagesOptions = Object.keys(allStages).map((x) => {
+  return { value: x, label: allStages[x] };
+});
+allStagesOptions.push({
+  value: "default",
+  label: "Back To All Students Details",
+});
 
-const styles = theme => ({
+const styles = (theme) => ({
   clear: {
-    clear: 'both'
-  }
-})
-
-
+    clear: "both",
+  },
+});
 
 export class AdmissionsDash extends React.Component {
-
   constructor(props) {
-
     super(props);
 
     if (this.props.match.params.dataType) {
       this.dataType = this.props.match.params.dataType;
     } else {
-      this.dataType = 'softwareCourse'
+      this.dataType = "softwareCourse";
     }
-    this.studentsURL = baseURL + 'students';
-    this.usersURL = baseURL + 'users/getall';
-    this.stage = null,
-    this.value = null,
-    this.loggedInUser = this.props.loggedInUser;
+    this.studentsURL = baseURL + "students";
+    this.usersURL = baseURL + "users/getall";
+    (this.stage = null),
+      (this.value = null),
+      (this.loggedInUser = this.props.loggedInUser);
 
     this.state = {
       data: [],
       sData: undefined, //subsetData,
-      fromDate: null
-    }
+      fromDate: null,
+    };
   }
 
   stageChangeEvent = (iData) => {
@@ -77,152 +81,166 @@ export class AdmissionsDash extends React.Component {
     let newData = this.state.data;
     newData[iData.rowId] = dataElem;
 
-    this.setState({ data: newData }, function () {
-    });
-  }
+    this.setState({ data: newData }, function () {});
+  };
 
-  changeDataType = option => {
+  changeDataType = (option) => {
     this.dataType = option.value;
     this.stage = null;
     this.value = null;
     this.fetchStudents();
-  }
+  };
 
-  changeStudentStage = option => {
-    this.value = { value: option.value, label: allStages[option.value] }
+  changeStudentStage = (option) => {
+    this.value = { value: option.value, label: allStages[option.value] };
     if (option.value === "default") {
       this.stage = null;
-      this.dataType = 'softwareCourse';
+      this.dataType = "softwareCourse";
       this.fetchStudents();
-      this.value = "Student Detials"
+      this.value = "Student Detials";
     } else {
       this.stage = option.value;
       this.fetchStudents();
-      this.dataType = 'softwareCourse';
+      this.dataType = "softwareCourse";
     }
-  }
+  };
 
   changeFromDate = async (date) => {
     await this.setState({
-      fromDate: date
-    })
+      fromDate: date,
+    });
     this.fetchStudents();
-  }
+  };
 
-  changeToDate = date => {
+  changeToDate = (date) => {
     this.toDate = date;
     this.fetchStudents();
-  }
+  };
 
   dataSetup = (data) => {
-    if (data.length > 0 ){
+    if (data.length > 0) {
       for (let i = 0; i < data.length; i++) {
-        data[i] = StudentService.dConvert(data[i])
+        data[i] = StudentService.dConvert(data[i]);
       }
-      const newData = data.map(v => ({ ...v, loggedInUser: this.props.loggedInUser.email.split('@')[0] }))
-      this.setState({ 'data': newData, fromDate: newData.slice(-1)[0].created_at }, function () {
-        this.props.fetchingFinish()
-      });
+      const newData = data.map((v) => ({
+        ...v,
+        loggedInUser: this.props.loggedInUser.email.split("@")[0],
+      }));
+      this.setState(
+        { data: newData, fromDate: newData.slice(-1)[0].created_at },
+        function () {
+          this.props.fetchingFinish();
+        }
+      );
     }
-  }
+  };
 
   render = () => {
-    const { classes, fetchPendingInterviewDetails } = this.props;  
-    const options = <Box>
-      <Select
-        className={"filterSelectGlobal"}
-        value={this.dataType}
-        onChange={this.changeDataType}
-        options={[{ value: "requestCallback", label: "Request Callback" },
-        { value: "softwareCourse", label: "Other Data" }]}
-        placeholder={"Select Data Type"}
-        isClearable={false}
-        components={animatedComponents}
-        closeMenuOnSelect={true}
-      />
-      <Select
-        className={"filterSelectGlobal"}
-        value={this.value}
-        onChange={this.changeStudentStage}
-        options={allStagesOptions}
-        placeholder={"Get Student Details By Stage"}
-        isClearable={false}
-        components={animatedComponents}
-        closeMenuOnSelect={true}
-      />
-
-      <MuiPickersUtilsProvider utils={DateFnsUtils}>
-        <KeyboardDatePicker
-          margin="dense"
-          style={{ marginLeft: 16, maxWidth: '40%' }}
-          value={this.state.fromDate}
-          id="date-picker-dialog"
-          label="From Date"
-          format="MM/dd/yyyy"
-          onChange={this.changeFromDate}
-          KeyboardButtonProps={{
-            'aria-label': 'change date',
-          }}
+    const { classes, fetchPendingInterviewDetails } = this.props;
+    const options = (
+      <Box>
+        <Select
+          className={"filterSelectGlobal"}
+          value={this.dataType}
+          onChange={this.changeDataType}
+          options={[
+            { value: "requestCallback", label: "Request Callback" },
+            { value: "softwareCourse", label: "Other Data" },
+          ]}
+          placeholder={"Select Data Type"}
+          isClearable={false}
+          components={animatedComponents}
+          closeMenuOnSelect={true}
+        />
+        <Select
+          className={"filterSelectGlobal"}
+          value={this.value}
+          onChange={this.changeStudentStage}
+          options={allStagesOptions}
+          placeholder={"Get Student Details By Stage"}
+          isClearable={false}
+          components={animatedComponents}
+          closeMenuOnSelect={true}
         />
 
-        <KeyboardDatePicker
-          margin="dense"
-          style={{ marginLeft: 16, maxWidth: '40%' }}
-          value={this.toDate}
-          id="date-picker-dialog"
-          label="To Date"
-          format="MM/dd/yyyy"
-          onChange={this.changeToDate}
-          KeyboardButtonProps={{
-            'aria-label': 'change date',
-          }}
-        />
-      </MuiPickersUtilsProvider>
-    </Box>;
-    
+        <MuiPickersUtilsProvider utils={DateFnsUtils}>
+          <KeyboardDatePicker
+            margin="dense"
+            style={{ marginLeft: 16, maxWidth: "40%" }}
+            value={this.state.fromDate}
+            id="date-picker-dialog"
+            label="From Date"
+            format="MM/dd/yyyy"
+            onChange={this.changeFromDate}
+            KeyboardButtonProps={{
+              "aria-label": "change date",
+            }}
+          />
+
+          <KeyboardDatePicker
+            margin="dense"
+            style={{ marginLeft: 16, maxWidth: "40%" }}
+            value={this.toDate}
+            id="date-picker-dialog"
+            label="To Date"
+            format="MM/dd/yyyy"
+            onChange={this.changeToDate}
+            KeyboardButtonProps={{
+              "aria-label": "change date",
+            }}
+          />
+        </MuiPickersUtilsProvider>
+      </Box>
+    );
+
     if (fetchPendingInterviewDetails) {
-      return  <MainLayout
-      columns={StudentService.columns[this.dataType]}
-      data={this.state.sData ? this.state.sData : this.state.data} />
+      return (
+        <MainLayout
+          columns={StudentService.columns[this.dataType]}
+          data={this.state.sData ? this.state.sData : this.state.data}
+        />
+      );
     }
-    
+
     if (!this.state.data.length) {
       return options;
     }
-    
-    return (<Box>
-      <MuiThemeProvider theme={theme}>
-        { this.props.fetchPendingInterviewDetails ? null : options}
-        <div className={classes.clear}></div>
-        <MainLayout
-          columns={StudentService.columns[this.dataType]}
-          data={this.state.sData ? this.state.sData : this.state.data} />
-      </MuiThemeProvider>
-    </Box>)
-  }
+
+    return (
+      <Box>
+        <MuiThemeProvider theme={theme}>
+          {this.props.fetchPendingInterviewDetails ? null : options}
+          <div className={classes.clear}></div>
+          <MainLayout
+            columns={StudentService.columns[this.dataType]}
+            data={this.state.sData ? this.state.sData : this.state.data}
+          />
+        </MuiThemeProvider>
+      </Box>
+    );
+  };
 
   componentDidMount() {
     this.fetchStudents();
     this.fetchUsers();
   }
 
-
   async fetchUsers() {
     try {
-      this.props.fetchingStart()
+      this.props.fetchingStart();
       const response = await axios.get(this.usersURL, {});
       this.props.usersSetup(response.data.data);
-      this.props.fetchingFinish()
+      this.props.fetchingFinish();
     } catch (e) {
-      console.log(e)
-      this.props.fetchingFinish()
+      console.log(e);
+      this.props.fetchingFinish();
     }
   }
 
   async fetchStudents() {
-    const { fetchPendingInterviewDetails, loggedInUser} = this.props; 
+    const { fetchPendingInterviewDetails, loggedInUser } = this.props;
     try {
-      this.props.fetchingStart()
+      this.props.fetchingStart();
       // response = ngFetch(this.studentsURL, 'GET', {
       //   params: {
       //     dataType: this.dataType,
@@ -232,38 +250,42 @@ export class AdmissionsDash extends React.Component {
       // }, true);
       let response;
       if (fetchPendingInterviewDetails) {
-         response = await axios.get(`${baseURL}students/pending_interview`, {
-           params: {
-             user: loggedInUser.mailId
-           }
-         });
+        response = await axios.get(`${baseURL}students/pending_interview`, {
+          params: {
+            user: loggedInUser.mailId,
+          },
+        });
       } else {
         response = await axios.get(this.studentsURL, {
           params: {
             dataType: this.dataType,
             stage: this.stage,
             from: this.state.fromDate,
-            to: this.toDate
-          }
+            to: this.toDate,
+          },
         });
       }
-      this.dataSetup(response.data.data)
+      this.dataSetup(response.data.data);
     } catch (e) {
-      console.log(e)
-      this.props.fetchingFinish()
+      console.log(e);
+      this.props.fetchingFinish();
     }
   }
 }
 
 const mapStateToProps = (state) => ({
   loggedInUser: state.auth.loggedInUser,
-  isAuthenticated: state.auth.isAuthenticated
+  isAuthenticated: state.auth.isAuthenticated,
 });
 
 const mapDispatchToProps = (dispatch) => ({
   fetchingStart: () => dispatch(changeFetching(true)),
   fetchingFinish: () => dispatch(changeFetching(false)),
-  usersSetup: (users) => dispatch(setupUsers(users))
+  usersSetup: (users) => dispatch(setupUsers(users)),
 });
 
-export default withRouter(withStyles(styles)(connect(mapStateToProps, mapDispatchToProps)(AdmissionsDash)))
+export default withRouter(
+  withStyles(styles)(
+    connect(mapStateToProps, mapDispatchToProps)(AdmissionsDash)
+  )
+);

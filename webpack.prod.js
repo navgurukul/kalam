9f3d1edc26db3d4439c3871dad9e8a6bc7bb4c98
pysaquib <iamsaquibnasim@gmail.com>
2020-12-09 16:09:59 +0530

    removed uglify

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,6 @@
 const merge = require("webpack-merge");
 const common = require("./webpack.common.js");
-const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+// const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 // const BrotliPlugin = require('brotli-webpack-plugin');
 const CompressionPlugin = require("compression-webpack-plugin");
 const webpack = require("webpack");
@@ -8,9 +8,9 @@ const webpack = require("webpack");
 module.exports = merge(common, {
   mode: "production",
   //   devtool: 'source-map'
-  optimization: {
-    minimizer: [new UglifyJsPlugin()],
-  },
+  // optimization: {
+  //   minimizer: [new UglifyJsPlugin()],
+  // },
   plugins: [
     new CompressionPlugin(),
     new webpack.DefinePlugin({

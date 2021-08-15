a73c4ef0202f910b77e53afd2413ba01d41d7160
komalng <komal@navgurukul.org>
2021-08-15 19:54:51 +0530

    Added.debounce function in search bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import IconButton from "@material-ui/core/IconButton";
 import TextField from "@material-ui/core/TextField";
 import InputAdornment from "@material-ui/core/InputAdornment";
+import { debounce } from "lodash";
 
 import SearchIcon from "@material-ui/icons/Search";
 
@@ -11,34 +12,27 @@ export default class Text extends Component {
     this.state = {
       name: "",
     };
+    this.handleSearchText = debounce(this.onSearchText, 500);
   }
   onChange = (event) => {
     this.setState({
       name: event.target.value,
     });
+    this.handleSearchText(this.state.name);
+  };
+  onSearchText = (input) => {
+    return this.props.searchByName(input);
   };
 
   render() {
     const { name } = this.state;
-    const {searchByName} = this.props
     return (
       <TextField
-      error={false}     
-       id="standard-basic"
+        error={false}
+        id="standard-basic"
         label="Search Name"
         value={name}
         onChange={this.onChange}
-        InputProps={{
-          endAdornment: (
-            <InputAdornment>
-              <IconButton>
-                {name.length > 0 && (
-                  <SearchIcon onClick={() => searchByName(name)} />
-                )}{" "}
-              </IconButton>
-            </InputAdornment>
-          ),
-        }}
       />
     );
   }

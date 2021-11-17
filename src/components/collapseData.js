78cdeccd1803534cda91815344fb428747058ff0
Vaibhav Magar <vaibhavmagar1901@gmail.com>
2021-11-17 12:14:56 +0530

    simple modal working fine

--- src/components/collapseData.js
+++ src/components/collapseData.js
@@ -8,8 +8,31 @@ import CardActions from "@material-ui/core/CardActions";
 import { Typography, CardContent } from "@material-ui/core";
 import { allStages } from "../config";
 
-const showContact = (student) => {
-  console.log(student, "I am studenttttt");
+import Box from "@material-ui/core/Box";
+import Button from "@material-ui/core/Button";
+import { Modal } from "@material-ui/core";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
+
+export const showContact = (student) => {
+  const [modalOpen, setModalOpen] = React.useState(false);
+  const handleOpen = () => {
+    setModalOpen(true);
+  };
+  const handleClose = () => {
+    setModalOpen(false);
+  };
+
   let studentStatus = "";
   if (student.status != null) {
     const status_student =
@@ -17,8 +40,7 @@ const showContact = (student) => {
     const status = status_student.split(/(?=[A-Z])/);
     studentStatus = status.join(" ");
   }
-
-  return (
+  return !modalOpen ? (
     <center>
       <ul>
         <li>
@@ -35,6 +57,7 @@ const showContact = (student) => {
             <a
               onClick={() => {
                 console.log("hello , ", student.id);
+                handleOpen();
               }}
               onMouseOver={(e) => {
                 e.target.style.color = "red";
@@ -56,91 +79,92 @@ const showContact = (student) => {
         <span>{student.status}</span>
       )}
     </center>
+  ) : (
+    <Modal
+      open={modalOpen}
+      onClose={handleClose}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <Box sx={style}>
+        <Typography id="modal-modal-title" variant="h6" component="h2">
+          {student.name}
+        </Typography>
+        <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+          {student.id}
+        </Typography>
+      </Box>
+    </Modal>
   );
 };
 
-class CollapseStudentData extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      expanded: false,
-    };
-  }
-
-  handleExpandClick = () => {
-    this.setState({
-      expanded: !this.state.expanded,
-    });
+const CollapseStudentData = (props) => {
+  const { classes, details, stage } = props;
+  const [expanded, setExpanded] = React.useState(false);
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
   };
-
-  render() {
-    const { classes, details, stage } = this.props;
-    console.log(details, "I am details");
-    const { expanded } = this.state;
-
-    if (!details.length) {
-      return (
-        <CardContent>
-          <center>
-            <Typography variant="h6">
-              {allStages[stage]}({details.length})
-            </Typography>
-          </center>
-          {details.length == 0 && (
-            <center>
-              <p style={{ fontSize: 15, color: "red" }}>No record Found</p>
-            </center>
-          )}
-        </CardContent>
-      );
-    }
-
+  if (!details.length) {
     return (
-      <div>
-        <CardContent>
+      <CardContent>
+        <center>
+          <Typography variant="h6">
+            {allStages[stage]}({details.length})
+          </Typography>
+        </center>
+        {details.length == 0 && (
           <center>
-            <Typography variant="h6">
-              {allStages[stage]}({details.length})
-            </Typography>
+            <p style={{ fontSize: 15, color: "red" }}>No record Found</p>
           </center>
-          {details.length > 0 &&
-            !expanded &&
-            details.slice(0, 10).map(showContact)}
-          {details.length > 0 && expanded && details.map(showContact)}
-        </CardContent>
-        <CardActions disableSpacing>
-          {details.length > 10 && (
-            <IconButton
-              className={clsx(classes.expand, {
-                [classes.expandOpen]: expanded,
-              })}
-              onClick={this.handleExpandClick}
-              aria-expanded={expanded}
-              aria-label="show more"
-            >
-              {expanded ? (
-                <div>
-                  <Typography>See Less</Typography>
-                  <ExpandLessIcon
-                    className={classes.expandOpen}
-                    color="primary"
-                  />
-                </div>
-              ) : (
-                <div>
-                  <Typography>See More</Typography>
-                  <ExpandMoreIcon
-                    className={classes.expandOpen}
-                    color="primary"
-                  />
-                </div>
-              )}
-            </IconButton>
-          )}
-        </CardActions>
-      </div>
+        )}
+      </CardContent>
     );
   }
-}
+  return (
+    <div>
+      <CardContent>
+        <center>
+          <Typography variant="h6">
+            {allStages[stage]}({details.length})
+          </Typography>
+        </center>
+        {details.length > 0 &&
+          !expanded &&
+          details.slice(0, 10).map(showContact)}
+        {details.length > 0 && expanded && details.map(showContact)}
+      </CardContent>
+      <CardActions disableSpacing>
+        {details.length > 10 && (
+          <IconButton
+            className={clsx(classes.expand, {
+              [classes.expandOpen]: expanded,
+            })}
+            onClick={handleExpandClick}
+            aria-expanded={expanded}
+            aria-label="show more"
+          >
+            {expanded ? (
+              <div>
+                <Typography>See Less</Typography>
+                <ExpandLessIcon
+                  className={classes.expandOpen}
+                  color="primary"
+                />
+              </div>
+            ) : (
+              <div>
+                <Typography>See More</Typography>
+                <ExpandMoreIcon
+                  className={classes.expandOpen}
+                  color="primary"
+                />
+              </div>
+            )}
+          </IconButton>
+        )}
+      </CardActions>
+    </div>
+  );
+};
 
 export default CollapseStudentData;

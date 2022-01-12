0f742d07e6c6f5f909bf6c7cc2510444f773b039
Vaibhav Magar <vaibhavmagar1901@gmail.com>
2022-01-12 22:07:45 +0530

    removed prior filter index

--- src/components/SelectReact.js
+++ src/components/SelectReact.js
@@ -15,16 +15,16 @@ export default class SelectReact extends React.Component {
   render() {
     const { options, onChange, filterList, index, column } = this.props;
     const { selectedOption } = this.state;
+
     return (
       <Select
         value={selectedOption}
         onChange={(event) => {
-          filterList[index - 1] = event.value;
+          filterList[index] = event.value;
           this.setState({ selectedOption: event });
-          onChange(filterList[index - 1], index - 1, column);
+          onChange(filterList[index], index, column);
         }}
         options={options}
-        value={this.state.selectedOption}
         styles={{
           menuList: (base) => ({
             ...base,

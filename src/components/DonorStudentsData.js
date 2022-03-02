fc234e6eb1bce456124587375617dac785f955b0
destro1108 <swanandev@gmail.com>
2022-03-02 16:42:49 +0530

    Conv to Fun Components 4

--- src/components/DonorStudentsData.js
+++ src/components/DonorStudentsData.js
@@ -1,63 +1,55 @@
 import React from "react";
-import axios from "axios";
-
 import StudentService from "../services/StudentService";
-
-import MainLayout from "./MainLayout";
-import { qualificationKeys, donor } from "../config";
+import { donor } from "../config";
 import DashboardPage from "./Dashboard";
 import SelectUiByButtons from "./SelectUiByButtons";
 import StudentsProgressCards from "./StudentsProgressCards";
 import GraphingPresentationJob from "./GraphingPresentationJob.js";
-const baseUrl = process.env.API_URL;
 
-class DonorStudentsData extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isShow: true,
-      donorName: donor.find(
-        (x) => x.id === parseInt(this.props.match.params.donorId)
-      ).name,
-    };
-  }
+const DonorStudentsData = (props) => {
+  const [state, setState] = React.useState({
+    isShow: true,
+    donorName: donor.find((x) => x.id === parseInt(props.match.params.donorId))
+      .name,
+  });
 
-  progressMade = (value) => {
-    this.setState({ isShow: value });
+  const progressMade = (value) => {
+    setState({ ...state, isShow: value });
   };
-  tabularData = (value) => {
-    this.setState({ isShow: value });
-  };
-  showGraphData = (value) => {
-    this.setState({ isShow: value });
-  };
-  render() {
-    const { donorName, isShow } = this.state;
-    const { donorId } = this.props.match.params;
 
-    return (
-      <div>
-        <SelectUiByButtons
-          name={`${donorName} Donor`}
-          progressMade={this.progressMade}
-          tabularData={this.tabularData}
-          showGraphData={this.showGraphData}
+  const tabularData = (value) => {
+    setState({ ...state, isShow: value });
+  };
+
+  const showGraphData = (value) => {
+    setState({ ...state, isShow: value });
+  };
+
+  const { donorName, isShow } = state;
+  const { donorId } = props.match.params;
+
+  return (
+    <div>
+      <SelectUiByButtons
+        name={`${donorName} Donor`}
+        progressMade={progressMade}
+        tabularData={tabularData}
+        showGraphData={showGraphData}
+      />
+      {isShow ? (
+        <DashboardPage
+          displayData={StudentService["DonorData"]}
+          url={`donor/${donorId}/students`}
         />
-        {isShow ? (
-          <DashboardPage
-            displayData={StudentService["DonorData"]}
-            url={`donor/${donorId}/students`}
-          />
-        ) : isShow === null ? (
-          <GraphingPresentationJob
-            url={`/donor/${donorId}/students/distribution`}
-          />
-        ) : (
-          <StudentsProgressCards url={`donor/${donorId}`} />
-        )}
-      </div>
-    );
-  }
-}
+      ) : isShow === null ? (
+        <GraphingPresentationJob
+          url={`/donor/${donorId}/students/distribution`}
+        />
+      ) : (
+        <StudentsProgressCards url={`donor/${donorId}`} />
+      )}
+    </div>
+  );
+};
 
 export default DonorStudentsData;
--- src/components/EditIcon.js
+++ src/components/EditIcon.js
@@ -1,44 +1,26 @@
-import React, { Component, Fragment } from "react";
+import React, { Fragment } from "react";
 import EditIcon from "@material-ui/icons/Edit";
 import { Dialog } from "@material-ui/core";
 import AddPartner from "./AddPartner";
 import DialogContent from "@material-ui/core/DialogContent";
 
-class EditPartnerDetails extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      dialogOpen: false,
-    };
-  }
-  handleClose = () => {
-    this.setState({
-      dialogOpen: false,
-    });
-  };
-  handleOpen = () => {
-    this.setState({
-      dialogOpen: true,
-    });
-  };
+const EditPartnerDetails = (props) => {
+  const [dialogOpen, setDialogOpen] = React.useState(false);
 
-  render() {
-    const { value } = this.props;
-    return (
-      <Fragment>
-        <EditIcon onClick={this.handleOpen} style={{ cursor: "pointer" }} />
-        <Dialog
-          scroll="paper"
-          open={this.state.dialogOpen}
-          onClose={this.handleClose}
-        >
-          <DialogContent dividers={true}>
-            <AddPartner value={value} />
-          </DialogContent>
-        </Dialog>
-      </Fragment>
-    );
-  }
-}
+  const handleClose = () => setDialogOpen(false);
+  const handleOpen = () => setDialogOpen(true);
+
+  const { value } = props;
+  return (
+    <Fragment>
+      <EditIcon onClick={handleOpen} style={{ cursor: "pointer" }} />
+      <Dialog scroll="paper" open={dialogOpen} onClose={handleClose}>
+        <DialogContent dividers={true}>
+          <AddPartner value={value} />
+        </DialogContent>
+      </Dialog>
+    </Fragment>
+  );
+};
 
 export default EditPartnerDetails;

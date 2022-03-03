e04eba3f49df13bd3389fe8bbca9a07ed6f92bd0
destro1108 <swanandev@gmail.com>
2022-03-03 17:52:17 +0530

    Conv to Fun components 10

--- src/components/OutreachDetails.js
+++ src/components/OutreachDetails.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useEffect } from "react";
 import axios from "axios";
-import { withStyles } from "@material-ui/core/styles";
+import { makeStyles } from "@material-ui/styles";
 import Container from "@material-ui/core/Container";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
@@ -9,7 +9,7 @@ import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
 
-const styles = (theme) => ({
+const useStyles = makeStyles(() => ({
   root: {
     display: "flex",
     overflowX: "hide",
@@ -23,87 +23,85 @@ const styles = (theme) => ({
   tableCell: {
     fontSize: "16px",
   },
-});
+}));
 
-export class OutreachDetails extends React.Component {
-  constructor(props) {
-    super(props);
+const OutreachDetails = () => {
+  const classes = useStyles();
+  const [state, setState] = React.useState({
+    outreachDetails: [],
+    listOfOutreachTeam: [],
+  });
 
-    this.state = {
-      outreachDetails: [],
-      listOfOutreachTeam: [],
-    };
-  }
-
-  componentDidMount() {
+  useEffect(() => {
     axios
       .get("https://join.navgurukul.org/api/outreach/records")
       .then((Response) => {
-        this.setState({
+        setState((prevState) => ({
+          ...prevState,
           outreachDetails: Response.data,
-        });
-        if (this.state.outreachDetails) {
-          this.setState({
-            listOfOutreachTeam: Object.keys(this.state.outreachDetails),
-          });
+        }));
+        if (state.outreachDetails) {
+          setState((prevState) => ({
+            ...prevState,
+            listOfOutreachTeam: Object.keys(prevState.outreachDetails),
+          }));
         }
       })
       .catch((error) => {
         console.error(error);
       });
-  }
+  }, []);
 
-  render() {
-    const { classes } = this.props;
-    return (
-      <>
-        <Container>
-          <Paper className={classes.root}>
-            <Table className={classes.table}>
-              <TableHead>
-                <TableRow>
-                  <TableCell className={classes.tableCell}>
-                    {" "}
-                    Outreach coordinator email
-                  </TableCell>
-                  <TableCell className={classes.tableCell}>
-                    Number of partners
-                  </TableCell>
-                  <TableCell className={classes.tableCell}>
-                    {" "}
-                    Number of students{" "}
-                  </TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {this.state.listOfOutreachTeam.map((item, index) => {
-                  let countOfStudent = 0;
-                  return (
-                    <TableRow key={index}>
-                      <TableCell className={classes.tableCell}>
-                        {item}
-                      </TableCell>
-                      <TableCell className={classes.tableCell}>
-                        {this.state.outreachDetails[item]["partners"].length}
-                      </TableCell>
-                      <TableCell className={classes.tableCell}>
-                        {this.state.outreachDetails[item]["partners"].forEach(
-                          (item) => {
-                            let values = Object.values(item)[0];
-                            countOfStudent = countOfStudent + values;
-                          }
-                        )}
-                        {countOfStudent}
-                      </TableCell>
-                    </TableRow>
-                  );
-                })}
-              </TableBody>
-            </Table>
-          </Paper>
-        </Container>
-      </>
-    );
-  }
-}
-export default withStyles(styles)(OutreachDetails);
+  console.log(state);
+
+  return (
+    <>
+      <Container>
+        <Paper className={classes.root}>
+          <Table className={classes.table}>
+            <TableHead>
+              <TableRow>
+                <TableCell className={classes.tableCell}>
+                  {" "}
+                  Outreach coordinator email
+                </TableCell>
+                <TableCell className={classes.tableCell}>
+                  Number of partners
+                </TableCell>
+                <TableCell className={classes.tableCell}>
+                  {" "}
+                  Number of students{" "}
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {state.listOfOutreachTeam.map((item, index) => {
+                let countOfStudent = 0;
+                console.log(countOfStudent);
+                return (
+                  <TableRow key={index}>
+                    <TableCell className={classes.tableCell}>{item}</TableCell>
+                    <TableCell className={classes.tableCell}>
+                      {state.outreachDetails[item]["partners"].length}
+                    </TableCell>
+                    <TableCell className={classes.tableCell}>
+                      {state.outreachDetails[item]["partners"].forEach(
+                        (item) => {
+                          let values = Object.values(item)[0];
+                          countOfStudent = countOfStudent + values;
+                        }
+                      )}
+                      {countOfStudent}
+                    </TableCell>
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+          </Table>
+        </Paper>
+      </Container>
+    </>
+  );
+};
+// export default withStyles(styles)(OutreachDetails);
+export default OutreachDetails;

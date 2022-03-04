70bbe22a48a23cea767fedf5d1146918aa85ee1e
destro1108 <swanandev@gmail.com>
2022-03-04 14:14:50 +0530

    Conv to Func Components 11

--- src/components/PartnerLink.js
+++ src/components/PartnerLink.js
@@ -1,24 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export class PartnerLink extends React.Component {
-  render = () => {
-    const { text } = this.props;
-
-    return (
-      <div>
-        <Link
-          target="_blank"
-          style={{ color: "#f05f40" }}
-          to={{
-            pathname: this.props.url,
-          }}
-        >
-          {text}
-        </Link>
-      </div>
-    );
-  };
-}
+const PartnerLink = (props) => {
+  const { text, url } = props;
+  return (
+    <div>
+      <Link
+        target="_blank"
+        style={{ color: "#f05f40" }}
+        to={{
+          pathname: url,
+        }}
+      >
+        {text}
+      </Link>
+    </div>
+  );
+};
 
 export default PartnerLink;

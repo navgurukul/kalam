d58c1bf5dc6460fda0877f148c48881972cfd245
destro1108 <swanandev@gmail.com>
2022-04-13 18:51:42 +0530

    Layout Footer Fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,34 @@
 import React from "react";
-import Typography from "@mui/material/Typography";
-import Box from "@mui/material/Box";
+import { Box, Typography } from "@mui/material";
+import { makeStyles } from "@mui/styles";
 
-export default () => (
-  <Box className="footer-container-box" p={1} mt={2}>
-    <Typography variant="body1" gutterBottom>
-      Managed and run by NavGurukul Students
-    </Typography>
-  </Box>
-);
+const useStyles = makeStyles((theme) => ({
+  root: {
+    position: "fixed",
+    bottom: 0,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100vw",
+    backgroundColor: "#f1f3f4",
+    textAlign: "center",
+    color: "black",
+    padding: theme.spacing(2),
+  },
+  dummySpace: {
+    height: theme.spacing(8),
+  },
+}));
+
+export default () => {
+  const classes = useStyles();
+  return (
+    <div className={classes.dummySpace}>
+      <Box className={classes.root}>
+        <Typography variant="body1" gutterBottom>
+          Managed and run by NavGurukul Students
+        </Typography>
+      </Box>
+    </div>
+  );
+};

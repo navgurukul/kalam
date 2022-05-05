a0ad93f5c3405ed5449be539d1df3a40bd9ced97
destro1108 <swanandev@gmail.com>
2022-05-05 16:43:32 +0530

    Revamped 404 Page!

--- src/components/layout/NotFoundPage.jsx
+++ src/components/layout/NotFoundPage.jsx
@@ -1,10 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Button, Container, Typography } from "@mui/material";
 
 const NotFoundPage = () => (
-  <div>
-    404 - <Link to="/">Go home</Link>
-  </div>
+  <Container
+    sx={{
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      alignItems: "center",
+      p: "0.4rem",
+    }}
+  >
+    <Typography variant="h3" textAlign="center" sx={{ marginBottom: "2rem" }}>
+      Oops! The Page you accessed does not exist
+    </Typography>
+    <Link to="/">
+      <Button size="large" variant="outlined" color="primary">
+        Go Home
+      </Button>
+    </Link>
+  </Container>
 );
 
 export default NotFoundPage;

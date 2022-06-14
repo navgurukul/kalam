1792a731ed4cfd2013031b43957894dbe084bdad
Vaibhav Magar <vaibhavmagar1901@gmail.com>
2022-06-15 00:35:21 +0530

    campusStatus : done

--- src/services/StudentService.jsx
+++ src/services/StudentService.jsx
@@ -1670,7 +1670,7 @@ const StudentService = {
     joinedDate,
     stageColumn,
     JobKabLagegiColumn,
-    CampusStatus,
+    // CampusStatus,
     ColumnUpload,
     daysPassedColumn,
     kitneAurDin,

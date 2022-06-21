9b8dda3bd288b4a076c2715946919f8c050dbeed
destro1108 <swanandev@gmail.com>
2022-06-21 17:01:19 +0530

    Changed Access for Update Partner

--- src/components/smallComponents/EditIcon.jsx
+++ src/components/smallComponents/EditIcon.jsx
@@ -1,10 +1,12 @@
 import React, { Fragment } from "react";
 import EditIcon from "@mui/icons-material/Edit";
-import { Dialog } from "@mui/material";
+import { Dialog, IconButton } from "@mui/material";
+import { useSelector } from "react-redux";
 import DialogContent from "@mui/material/DialogContent";
 import AddPartner from "../partner/AddPartner";
 
 const EditPartnerDetails = (props) => {
+  const { privileges } = useSelector((state) => state.auth);
   const [dialogOpen, setDialogOpen] = React.useState(false);
 
   const handleClose = () => setDialogOpen(false);
@@ -13,7 +15,15 @@ const EditPartnerDetails = (props) => {
   const { value } = props;
   return (
     <>
-      <EditIcon onClick={handleOpen} style={{ cursor: "pointer" }} />
+      <IconButton
+        disabled={
+          !privileges.some((priv) => priv.privilege === "UpdatePartner")
+        }
+        onClick={handleOpen}
+      >
+        <EditIcon />
+      </IconButton>
+
       <Dialog scroll="paper" open={dialogOpen} onClose={handleClose}>
         <DialogContent dividers>
           <AddPartner partnerId={value} closeDialog={handleClose} />

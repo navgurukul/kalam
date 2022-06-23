08247ee9b3b3ae20e79c7571ab0bbd11b79088cd
Vaibhav Magar <vaibhavmagar1901@gmail.com>
2022-06-23 13:08:43 +0530

    snackbar added

--- src/components/smallComponents/DeleteStudent.jsx
+++ src/components/smallComponents/DeleteStudent.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import axios from "axios";
 import DeleteIcon from "@mui/icons-material/Delete";
 import { useDispatch, useSelector } from "react-redux";
+import { useSnackbar } from "notistack";
 import { setStudentData } from "../../store/slices/studentSlice";
 
 const baseUrl = import.meta.env.VITE_API_URL;
 
 const DeleteStudent = (props) => {
+  const snackbar = useSnackbar();
   const { studentId } = props;
 
   const { studentData, totalData } = useSelector((state) => state.students);
@@ -22,6 +24,9 @@ const DeleteStudent = (props) => {
       axios
         .delete(`${baseUrl}students/${studentId}`)
         .then(() => {
+          snackbar.enqueueSnackbar("Student deleted successfully", {
+            variant: "success",
+          });
           const newStudentData = studentData.filter(
             (student) => student.id !== studentId
           );
@@ -30,7 +35,11 @@ const DeleteStudent = (props) => {
             totalData: totalData - 1,
           });
         })
-        .catch(() => {});
+        .catch(() => {
+          snackbar.enqueueSnackbar("Error deleting student", {
+            variant: "error",
+          });
+        });
     }
   };
 

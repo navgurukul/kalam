f86cb7fdbb0526217ff231a88b7dce52997b886e
destro1108 <swanandev@gmail.com>
2022-06-24 18:00:58 +0530

    Modified NoAccessMessage

--- src/components/layout/NotHaveAccess.jsx
+++ src/components/layout/NotHaveAccess.jsx
@@ -1,25 +1,48 @@
+import { Typography } from "@mui/material";
+import { Box } from "@mui/system";
 import React from "react";
 
 const NotHaveAccess = () => (
-  <div
+  <Box
     style={{
       display: "flex",
+      flexDirection: "column",
       justifyContent: "center",
       alignItems: "center",
     }}
   >
-    <h1
-      style={{
-        color: "red",
-        fontSize: "50px",
-        fontWeight: "bold",
-        textAlign: "center",
-        marginTop: "50px",
+    <Typography
+      variant="h3"
+      color="error"
+      sx={{
+        marginTop: "4rem",
       }}
     >
       You need access to view this page!!!
-    </h1>
-  </div>
+    </Typography>
+    <Typography
+      variant="h4"
+      sx={{
+        marginTop: "0.8rem",
+      }}
+    >
+      For Requesting Access, contact the following people
+    </Typography>
+    <Box
+      sx={{
+        marginTop: "1.2rem",
+        display: "flex",
+        flexDirection: "column",
+        textAlign: "center",
+      }}
+    >
+      {["Kittiy P", "Kirithiv R", "Swanand Buva", "Vaibhav Magar"].map((nm) => (
+        <Typography key={nm} variant="h6">
+          {nm}
+        </Typography>
+      ))}
+    </Box>
+  </Box>
 );
 
 export default NotHaveAccess;

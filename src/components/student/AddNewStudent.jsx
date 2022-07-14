77f365b8694dfc1a3c3023f9a8cfaaff4ed2a3d6
destro1108 <swanandev@gmail.com>
2022-07-14 15:59:28 +0530

    Added react-hook-form

--- src/components/student/AddNewStudent.jsx
+++ src/components/student/AddNewStudent.jsx
@@ -1,14 +1,61 @@
-import { Container, Grid, Typography } from "@mui/material";
+import { Container, Grid, TextField, Typography } from "@mui/material";
 import React from "react";
+import { Controller, useForm } from "react-hook-form";
 
 const AddNewStudent = () => {
-  const [studentData, setStudentData] = React.useState({});
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    control,
+    setValue,
+    watch,
+    reset,
+  } = useForm();
+
+  const [studentData, setStudentData] = React.useState({
+    fullName: "",
+    email: "",
+    whatsapp: "",
+    alternateNumber: "",
+    gender: "",
+    dob: "",
+    district: "",
+    pinCode: "",
+    state: "",
+    city: "",
+    currentStatus: "",
+    qualification: "",
+    schoolMedium: "",
+    caste: "",
+    religion: "",
+    percentageIn10th: "",
+    percentageIn12th: "",
+  });
   return (
     <Container maxWidth="md">
       <Typography variant="h4">Add New Student Data</Typography>
       <Grid container spacing={2}>
         <Grid item xs={12}>
-          1
+          <Controller
+            control={control}
+            name="name"
+            defaultValue={studentData.fullName}
+            render={({ field: { ref, ...rest } }) => (
+              <TextField
+                variant="outlined"
+                id="MiddleName"
+                inputRef={ref}
+                label="Full Name"
+                placeholder="Full Name"
+                autoComplete="off"
+                error={!!errors.name}
+                type="text"
+                helperText={errors.name ? "Enter Full Name" : "Ex. PQR"}
+                {...rest}
+              />
+            )}
+          />
         </Grid>
       </Grid>
     </Container>

f364725ac3fbe38396085c2218484794495f6304
destro1108 <swanandev@gmail.com>
2022-07-28 17:23:09 +0530

    Revamped OwnerSchedule & Refactored Code

--- src/components/owner/OwnerSchedule.jsx
+++ src/components/owner/OwnerSchedule.jsx
@@ -1,28 +1,27 @@
-import { Card, Grid, Modal, TextField } from "@mui/material";
-import { DatePicker, LocalizationProvider } from "@mui/lab";
 import React, { useEffect, useState } from "react";
-import DateFnsUtils from "@mui/lab/AdapterDateFns";
+import { Card, Modal, TextField, Typography } from "@mui/material";
+import { DatePicker, LocalizationProvider } from "@mui/lab";
+import AdapterDayjs from "@mui/lab/AdapterDayjs";
+import axios from "axios";
+import dayjs from "dayjs";
+import { Box } from "@mui/system";
 import MainLayout from "../muiTables/MainLayout";
 
 const baseUrl = import.meta.env.VITE_API_URL;
 function OwnerSchedule(props) {
-  const { ScheduleOpen, setScheduleOpen } = props;
+  const { scheduleOpen, setScheduleOpen } = props;
   const columns = [
     {
       name: "name",
-      label: "name",
+      label: "Name",
       options: {
         filter: false,
         sort: false,
-        customBodyRender: React.useCallback(
-          (value) => <p>{value.split("T")[0]}</p>,
-          []
-        ),
       },
     },
     {
       name: "phone",
-      label: "Phone Number",
+      label: "Phone No.",
       options: {
         filter: false,
         sort: false,
@@ -38,33 +37,42 @@ function OwnerSchedule(props) {
     },
     {
       name: "start_time",
-      label: "start time",
+      label: "Start Time",
       options: {
         filter: false,
         sort: false,
+        customBodyRender: (value) => dayjs(value, "hh:mm").format("hh:mm A"),
       },
     },
     {
       name: "end_time_expected",
-      label: "end time",
+      label: "Expected End Time",
       options: {
         filter: false,
         sort: false,
+        customBodyRender: (value) => dayjs(value, "hh:mm").format("hh:mm A"),
       },
     },
     {
       name: "on_date",
-      label: "date",
+      label: "Date",
       options: {
         filter: true,
         sort: true,
-        // eslint-disable-next-line react/no-unstable-nested-components
-        customBodyRender: (value) => <p>{value.split("T")[0]}</p>,
+        customBodyRender: (value) => dayjs(value).format("D MMM YYYY"),
       },
     },
     {
       name: "owner",
-      label: "owner Name",
+      label: "Owner Name",
+      options: {
+        filter: true,
+        sort: true,
+      },
+    },
+    {
+      name: "topic_name",
+      label: "Topic Name",
       options: {
         filter: true,
         sort: true,
@@ -73,85 +81,61 @@ function OwnerSchedule(props) {
   ];
   const [slotData, setSlotData] = useState([]);
   const [date, setDate] = useState(Date.now);
-  const handleDateChange = (dater) => {
-    //console.log(dater);
-    setDate(dater);
-    // let DateArray = typeof dater;
-    const d = new Date(dater).split(" ");
-    d[2] += ",";
-    // eslint-disable-next-line no-use-before-define
-    const DateToSend = [d[3], month[d[1]], d[2]].join("-").replace(",", "");
-    fetch(`${baseUrl}/slot/interview/ondate/${DateToSend}`).then((res) => {
-      res.json().then((data) => {
-        const dataAdd = data.data.map((item) => ({
+
+  const handleDateChange = (newDate) => {
+    const d = dayjs(newDate);
+    if (!d.isValid()) return;
+    const DateToSend = d.format("YYYY-MM-DD");
+    axios
+      .get(`${baseUrl}/slot/interview/ondate/${DateToSend}`)
+      .then(({ data }) => {
+        const newSlotData = data.data.map((item) => ({
           ...item,
-          on_date: item.on_date.split("T")[0],
-          start_time: item.start_time,
-          end_time_expected: item.end_time_expected,
-          topic_name: item.topic_name,
           name: item.student.name,
           email: item.student.email,
           phone: item.contacts.mobile,
-          owner: item.user?.user_name,
+          owner: item.user?.user_name || "",
         }));
-
-        setSlotData(dataAdd);
+        setDate(DateToSend);
+        setSlotData(newSlotData);
       });
-    });
-    useEffect(() => {
-      handleDateChange(date);
-    }, []);
   };
-  const month = {
-    Jan: `01`,
-    Feb: `02`,
-    Mar: `03`,
-    Apr: `04`,
-    May: `05`,
-    Jun: `06`,
-    Jul: `07`,
-    Aug: `08`,
-    Sep: `09`,
-    Oct: `10`,
-    Nov: `11`,
-    Dec: `12`,
-  };
+
+  useEffect(() => {
+    handleDateChange(date);
+  }, []);
+
   return (
     <Modal
-      open={ScheduleOpen}
-      onClose={() => {
-        setScheduleOpen(false);
-      }}
-      style={{
+      open={scheduleOpen}
+      onClose={() => setScheduleOpen(false)}
+      sx={{
         height: "85%",
-        marginTop: "1%",
+        marginTop: "1.4rem",
+        width: "99%",
+        mx: "1.2rem",
         overflow: "scroll",
       }}
     >
-      <Card>
-        <h1>
-          <center>Interview Schedule</center>
-        </h1>
-        <LocalizationProvider dateAdapter={DateFnsUtils}>
-          <Grid container justify="space-around">
-            <DatePicker
-              margin="normal"
-              id="date-picker-dialog"
-              format="yyyy-MM-dd"
-              value={date}
-              onChange={(dates) => {
-                handleDateChange(dates);
-                //console.log(dates);
-              }}
-              inputVariant="outlined"
-              KeyboardButtonProps={{
-                "aria-label": "change date",
-              }}
-              renderInput={(params) => <TextField {...params} />}
-            />
-          </Grid>
-        </LocalizationProvider>
-        <MainLayout title="Schedule" columns={columns} data={slotData} />
+      <Card sx={{ py: "1.2rem", px: "0.8rem" }}>
+        <Typography variant="h3" textAlign="center" fontWeight="medium">
+          Interview Schedule
+        </Typography>
+        <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
+          <Box sx={{ display: "flex", justifyContent: "center" }}>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <DatePicker
+                margin="normal"
+                format="YYYY-MM-DD"
+                value={date}
+                onChange={(newDate) => handleDateChange(newDate)}
+                inputVariant="outlined"
+                renderInput={(params) => <TextField {...params} />}
+              />
+            </LocalizationProvider>
+          </Box>
+          <MainLayout title="Schedule" columns={columns} data={slotData} />
+        </Box>
       </Card>
     </Modal>
   );

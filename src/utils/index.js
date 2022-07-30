5f6916af71c5300df1189a585e8f7be639c43c91
destro1108 <swanandev@gmail.com>
2022-07-30 16:33:55 +0530

    Fixed Marks issue

--- src/utils/index.js
+++ src/utils/index.js
@@ -13,14 +13,13 @@ export const dConvert = (data) => {
   }
 
   x.altNumber = x.contacts[0]?.alt_mobile || "";
-
   x.gender =
     x.gender === 1 ? "Female" : x.gender === 2 ? "Male" : "Transgender";
   x.stage = allStages[x.stage];
-  x.marks = x.enrolmentKey[x.enrolmentKey.length - 1]
-    ? parseInt(x.enrolmentKey[x.enrolmentKey.length - 1].total_marks, 10)
+  x.marks = x.enrolmentKey.length
+    ? parseInt(x.enrolmentKey[0].total_marks, 10)
     : null;
-  x.marks = isNaN(x.marks) ? null : x.marks;
+  x.marks = isNaN(x.marks) ? "N/A" : x.marks;
   x.lastUpdated = x.lastTransition ? x.lastTransition.created_at : null;
   x.age = x.dob ? new Date().getFullYear() - +x.dob.slice(0, 4) : "NA";
   x.studentOwner = x.feedbacks ? x.feedbacks.to_assign : x.to_assign;

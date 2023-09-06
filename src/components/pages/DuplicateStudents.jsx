b09a12d230b2aa78d25f591c73d7104360db3573
Poonam-Singh-Bagh <poonambagh98@gmail.com>
2023-09-06 13:58:59 +0530

    Removed the book slot column

--- src/components/pages/DuplicateStudents.jsx
+++ src/components/pages/DuplicateStudents.jsx
@@ -109,36 +109,36 @@ const DuplicateStudents = () => {
         customBodyRender: (value) => allStages[value],
       },
     },
-    {
-      name: "stage",
-      label: "Book Slot",
-      options: {
-        filter: false,
-        customBodyRender: React.useCallback(
-          (_, rowMeta) => (
-            <Button
-              // disabled={
-              //   rowMeta.rowData[1] !== "pendingEnglishInterview" &&
-              //   rowMeta.rowData[1] !== "pendingCultureFitInterview" &&
-              //   rowMeta.rowData[1] !== "pendingAlgebraInterview"
-              // }
-              disabled="true"
-              variant="contained"
-              color="primary"
-              style={{ fontSize: "10px" }}
-              onClick={() => {
-                navigate({
-                  pathname: `/bookSlot/${rowMeta.rowData[0]}`,
-                });
-              }}
-            >
-              Book Slot
-            </Button>
-          ),
-          []
-        ),
-      },
-    },
+    // {
+    //   name: "stage",
+    //   label: "Book Slot",
+    //   options: {
+    //     filter: false,
+    //     customBodyRender: React.useCallback(
+    //       (_, rowMeta) => (
+    //         <Button
+    //           // disabled={
+    //           //   rowMeta.rowData[1] !== "pendingEnglishInterview" &&
+    //           //   rowMeta.rowData[1] !== "pendingCultureFitInterview" &&
+    //           //   rowMeta.rowData[1] !== "pendingAlgebraInterview"
+    //           // }
+    //           disabled="true"
+    //           variant="contained"
+    //           color="primary"
+    //           style={{ fontSize: "10px" }}
+    //           onClick={() => {
+    //             navigate({
+    //               pathname: `/bookSlot/${rowMeta.rowData[0]}`,
+    //             });
+    //           }}
+    //         >
+    //           Book Slot
+    //         </Button>
+    //       ),
+    //       []
+    //     ),
+    //   },
+    // },
     {
       name: "total_marks",
       label: "Marks",

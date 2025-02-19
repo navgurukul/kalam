223ff00a5812ecefbbb05d6f331093ba70a0bbd7
its-nasir <nasirmoh8802@gmail.com>
2025-02-19 11:36:26 +0530

    gh-pages update

---
+++ assets/AssessmentAttempts.f00c0721.js
@@ -0,0 +1,1 @@
+import{bG as t,b9 as e,bi as a}from"./vendor.643cae97.js";import"./vendor_core_js_pure.66ec04e9.js";const n=()=>{const{assessmentId:s}=t();return e(a,{children:["Hi ",s]})};export{n as default};
